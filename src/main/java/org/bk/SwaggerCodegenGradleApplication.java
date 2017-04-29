69991e66aa3d4da44aca8f4a390441d8dbd30512
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-04-28 21:32:21 -0700

    Swagger Codegen integrated with Gradle

---
+++ src/main/java/org/bk/SwaggerCodegenGradleApplication.java
@@ -0,0 +1,12 @@
+package org.bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCodegenGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCodegenGradleApplication.class, args);
+	}
+}
